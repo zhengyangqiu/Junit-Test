14d3102e437d415f603348c3cf509717a5ec6b33
zhengyang qiu <15224003720@163.com>
2022-07-23 23:54:51 +0100

    add disabled method

--- src/main/java/com/zhengyang/mockito/MathUtils.java
+++ src/main/java/com/zhengyang/mockito/MathUtils.java
@@ -5,6 +5,17 @@ public class MathUtils {
         return a+b;
 
     }
+    public int subtract(int a, int b){
+        return a -b ;
+    }
+    public int multiple(int a, int b){
+        return a * b;
+    }
+    public int divide(int a, int b){
+        return a / b;
+    }
+
+
     public double computerCircleArea(double radius){
         return radius*radius*Math.PI;
 
--- src/test/java/com/zhengyang/mockito/MathUtilsTest.java
+++ src/test/java/com/zhengyang/mockito/MathUtilsTest.java
@@ -1,14 +1,33 @@
 package com.zhengyang.mockito;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//only create instance for once no matter the number of methods
 class MathUtilsTest {
+    //static method  has no  dependency on the instance, we can run static method before mathutilstest instance created
+    @BeforeAll
+    static void beforeAllInt(){
+        System.out.println("This needs to run before all ");
+    }
+    MathUtils mathUtils;
+    @BeforeEach
+    void init(){
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void cleanup(){
+        System.out.println("cleaning up...");
+    }
 
     @Test
+    @DisplayName("test add method")
     void add() {
-        MathUtils mathUtils = new MathUtils();
+
         int expected = 2;
         int actual = mathUtils.add(1,1);
         assertEquals(expected,actual,"The add method should add two numbers");
@@ -27,8 +46,21 @@ class MathUtilsTest {
 
     @Test
     void testComputerCircleRadius(){
-        MathUtils mathUtils = new MathUtils();
+
         assertEquals(314.1592653589793, mathUtils.computerCircleArea(10),"should return right circle area");
     }
+    @Test
+    void testDivide(){
 
+        assertThrows(ArithmeticException.class,()-> mathUtils.divide(1,0),"divide by zero should throw");
+
+
+
+    }
+    @Test
+    @Disabled
+    @DisplayName("TDD method. should not run")
+    void testDisabled(){
+        fail("This test should be disabled");
+    }
 }
